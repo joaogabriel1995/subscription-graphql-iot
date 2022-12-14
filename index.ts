531181272c5d524f13f7c032961b1154822f9e30
joaogabriel1995 <joaogabrielrp@gmail.com>
2022-12-13 21:33:34 -0300

    Ajuste para comportar webSocket

--- index.ts
+++ index.ts
@@ -2,21 +2,52 @@ import 'reflect-metadata'
 
 import path from 'path'
 import { ApolloServer } from 'apollo-server'
+import { useServer } from 'graphql-ws/lib/use/ws'
 import { buildSchema } from 'type-graphql'
 import { UserResolver } from './src/resolvers/UserResolver'
+import cors from 'cors'
+import express from 'express'
+import { WebSocketServer } from 'ws'
+import { ApolloServerPluginDrainHttpServer } from 'apollo-server-core'
+import bodyParser from 'body-parser'
+import { createServer } from 'http'
 
 async function main() {
   const schema = await buildSchema({
     resolvers: [UserResolver],
     emitSchemaFile: path.resolve(__dirname, 'schema.gql')
   })
+  const app = express()
+  const httpServer = createServer(app)
 
-  const server = new ApolloServer({
-    schema
+  const wsServer = new WebSocketServer({
+    server: httpServer,
+    path: '/graphql'
   })
 
-  const { url } = await server.listen()
+  const serverCleanup = useServer({ schema }, wsServer)
 
-  console.log(`Server running on ${url}`)
+  const server = new ApolloServer({
+    schema,
+    plugins: [
+      ApolloServerPluginDrainHttpServer({ httpServer }),
+      {
+        async serverWillStart() {
+          return {
+            async drainServer() {
+              await serverCleanup.dispose()
+            }
+          }
+        }
+      }
+    ]
+  })
+  await server.listen()
+  app.use('/graphql', cors<cors.CorsRequest>(), bodyParser.json())
+  const PORT = 4001
+  // Now that our HTTP server is fully set up, we can listen to it.
+  httpServer.listen(PORT, () => {
+    console.log(`Server is now running on http://localhost:${PORT}/graphql`)
+  })
 }
 main()
